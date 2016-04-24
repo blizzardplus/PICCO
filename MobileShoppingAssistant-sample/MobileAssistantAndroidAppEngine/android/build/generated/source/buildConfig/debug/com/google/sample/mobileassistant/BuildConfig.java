e09fe824f441d45042aa3c50642b60af318919f0
Yaosheng Fu <yfu@princeton.edu>
2016-04-23 21:44:49 -0400

    Add the MobileShopping App

---
+++ MobileShoppingAssistant-sample/MobileAssistantAndroidAppEngine/android/build/generated/source/buildConfig/debug/com/google/sample/mobileassistant/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.sample.mobileassistant;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.sample.mobileassistant";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final float DUMMY_LATITUDE = 37.22222f;
+  public static final float DUMMY_LONGITUDE = -122.11111f;
+  public static final String ROOT_URL = "https://bright-zodiac-128723.appspot.com/_ah/api/";
+  public static final String SENDER_ID = "811318688635";
+  public static final boolean SIGN_IN_REQUIRED = true;
+  public static final String WEB_CLIENT_ID = "811318688635-r5uoj44gbbl916fniejig1g346k2eqms.apps.googleusercontent.com";
+}
