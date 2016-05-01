02f08dbd44153d3ce60581fb911cf997d6b56ed5
Mohammad Shahrad <mshahrad@princeton.edu>
2016-05-01 01:16:33 -0400

    delay added for the logo activity instead of click

--- MobileShoppingAssistant-sample/MobileAssistantAndroidAppEngine/android/src/main/java/com/google/sample/mobileassistant/LogoActivity.java
+++ MobileShoppingAssistant-sample/MobileAssistantAndroidAppEngine/android/src/main/java/com/google/sample/mobileassistant/LogoActivity.java
@@ -3,6 +3,7 @@ package com.google.sample.mobileassistant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 public class LogoActivity extends Activity {
@@ -11,11 +12,20 @@ public class LogoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(LogoActivity.this, SignInActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
     }
 
-    public void goToLogin(View view){
-        Intent intent = new Intent(this, SignInActivity.class);
-        startActivity(intent);
-    }
+//    public void goToLogin(View view){
+//        Intent intent = new Intent(this, SignInActivity.class);
+//        startActivity(intent);
+//    }
 
 }
