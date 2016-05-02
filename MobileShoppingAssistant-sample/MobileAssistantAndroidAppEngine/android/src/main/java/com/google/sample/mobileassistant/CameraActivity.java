995e48b8aabb107825716fa3b58052b00d638188
Mohammad Shahrad <mshahrad@princeton.edu>
2016-05-02 17:00:05 -0400

    drawer added

--- MobileShoppingAssistant-sample/MobileAssistantAndroidAppEngine/android/src/main/java/com/google/sample/mobileassistant/CameraActivity.java
+++ MobileShoppingAssistant-sample/MobileAssistantAndroidAppEngine/android/src/main/java/com/google/sample/mobileassistant/CameraActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import java.io.File;
@@ -25,7 +29,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -34,15 +40,34 @@ import android.widget.VideoView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
-public class CameraActivity extends ActionBarActivity implements View.OnClickListener {
+public class CameraActivity extends ActionBarActivity
+        implements View.OnClickListener, NavigationDrawerFragment.NavigationDrawerCallbacks {
     private Button scanBtn;
     static final int REQUEST_TAKE_PHOTO = 1;
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private DrawerLayout mDrawerLayout;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout_camera));
+
         scanBtn = (Button)findViewById(R.id.scan_receipt_button);
         scanBtn.setOnClickListener(this);
     }
@@ -160,5 +185,100 @@ public class CameraActivity extends ActionBarActivity implements View.OnClickLis
         }
     }
 
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                Intent intent1 = new Intent(this, MainActivity.class);
+                startActivity(intent1);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                Intent intent2 = new Intent(this, BarcodeActivity.class);
+                startActivity(intent2);
+                break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                Intent intent3 = new Intent(this, CameraActivity.class);
+                startActivity(intent3);
+                break;
+            case 6:
+                mTitle = getString(R.string.title_section6);
+                break;
+            case 8:
+                mTitle = getString(R.string.title_section8);
+                Intent intent7 = new Intent(this, NearActivity.class);
+                startActivity(intent7);
+                break;
+            case 9:
+                mTitle = getString(R.string.title_section9);
+                Intent intent8 = new Intent(this, SettingsActivity.class);
+                startActivity(intent8);
+                break;
+            case 10:
+                mTitle = getString(R.string.title_section10);
+                SignInActivity.onSignOut(this);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setBackgroundDrawable(new ColorDrawable(0xff8c1a));
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((CameraActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
 
 }
